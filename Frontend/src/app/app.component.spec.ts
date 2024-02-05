0c8c4c5470eefef6d17a61e9665b190163326794
Mohamed Amine Bensbaho <mamine@hiberus.com>
2024-02-05 12:01:45 +0100

    Backend y configuración básica del frontend

--- Frontend/src/app/app.component.spec.ts
+++ Frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Frontend' title`, () => {
+  it(`should have the 'Gestion Clientes' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Frontend');
+    expect(app.title).toEqual('Gestion Clientes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Gestion Clientes');
   });
 });
