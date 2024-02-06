f68129b465d2cceae8084facf39be8eea9800d2e
joroz10 <javieroroz03@gmail.com>
2024-02-06 14:26:40 +0100

    Mejora slidebar

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, SideMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
