c92d7d55c953c8c470e623169af4fb4b0ee3691d
Xabier <xabiermuro1612@gmail.com>
2024-02-07 12:12:11 +0100

    tree-menu

--- Frontend/src/app/dashboard/dashboard.component.ts
+++ Frontend/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { SideMenuComponent } from '../side-menu/side-menu.component';
+import { TreeMenuComponent } from '../tree-menu/tree-menu.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HeaderComponent, SideMenuComponent],
+  imports: [HeaderComponent, SideMenuComponent,TreeMenuComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
---
+++ Frontend/src/app/tree-menu/tree-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tree-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './tree-menu.component.html',
+  styleUrl: './tree-menu.component.css'
+})
+export class TreeMenuComponent {
+
+}
