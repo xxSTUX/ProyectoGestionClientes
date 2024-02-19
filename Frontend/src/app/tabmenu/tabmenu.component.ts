9fb44eedbc073a3ef1e1dd2288225b833cfd0c03
JavierArtazcoz <jartazcoz@hiberus.com>
2024-02-19 13:17:24 +0100

    Pantalla crear seguimientos meojrada
    Falta el tree para terminarla

--- Frontend/src/app/tabmenu/tabmenu.component.ts
+++ Frontend/src/app/tabmenu/tabmenu.component.ts
@@ -4,19 +4,30 @@ import { SideMenuComponent } from '../side-menu/side-menu.component';
 import { HeaderComponent } from '../header/header.component';
 import { HttpClientModule} from '@angular/common/http';
 import { AngularEditorConfig, AngularEditorModule } from '@kolkov/angular-editor';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tabmenu',
   standalone: true,
-  imports: [HeaderComponent, SideMenuComponent, TreeMenuComponent,HttpClientModule, AngularEditorModule],
+  imports: [HeaderComponent, SideMenuComponent, TreeMenuComponent,HttpClientModule, AngularEditorModule, FormsModule],
   templateUrl: './tabmenu.component.html',
   styleUrl: './tabmenu.component.css'
 })
 export class TabmenuComponent {
+
+  id:string = '';
+  fechaCre: string = '';
+  fechaMod: string = '';
+  usuario: string = '';
+  usuarioMod: string = '';
+  nombre: string = '';
+  texto: string= '';
+
   config: AngularEditorConfig = {
     editable: true,
     spellcheck: true,
-    height: '15rem',
+    height: '32vh',
     minHeight: '5rem',
     placeholder: 'Enter text here...',
     translate: 'no',
@@ -24,5 +35,17 @@ export class TabmenuComponent {
     defaultFontName: 'Arial',
    
   };
+
+  public create(){
+    alert("Llamar a funcion: createSeguimiento("+this.id+","+this.usuario+","+this.nombre+","+this.texto);
+  }
+
+  public delete(){
+
+  }
+
+  public cancel(){
+
+  }
 
 }
