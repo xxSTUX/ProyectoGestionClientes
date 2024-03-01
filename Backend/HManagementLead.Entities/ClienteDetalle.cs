0fa93eede7f67266903a207399f865bd6b8d5550
Gianfranco Javier Álvarez Valencia <gjalvarez@hiberus.com>
2024-03-01 12:04:18 +0100

    Cambio BackEnd; Mostrar Ids de la entidades devueltas en el JSON que va al Arbol de navegación-->Comntar los JSONIGNORE

--- Backend/HManagementLead.Entities/ClienteDetalle.cs
+++ Backend/HManagementLead.Entities/ClienteDetalle.cs
@@ -12,7 +12,7 @@ namespace HManagementLead.Entities
         public IList<TablaIntermedia> SeguimientoCliente { get; set; } = new List<TablaIntermedia>();
         //public virtual IList<SeguimientoDetalle> Seguimientos { get; set; } = new List<SeguimientoDetalle>();
         //public virtual IList<LicitacionDetalle> licitaciones { get; set; } = new List<LicitacionDetalle>();
-        [JsonIgnore]
+        //[JsonIgnore]
         public int Id { get; set; }
         public string Nombre { get; set; } = null!;
         public IList<ProyectoDetalle> Proyectos { get; set; } = new List<ProyectoDetalle>();
