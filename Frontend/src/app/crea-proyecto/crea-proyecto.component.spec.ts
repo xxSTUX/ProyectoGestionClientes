db24a93d83d0ba2ca46ed631877fc598ed9510ce
Xabier <xabiermuro1612@gmail.com>
2024-03-08 14:30:05 +0100

    crear proyecto hacido

--- Frontend/src/app/crea-proyecto/crea-proyecto.component.spec.ts
+++ Frontend/src/app/crea-proyecto/crea-proyecto.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreaProyectoComponent } from './crea-proyecto.component';
 
+
 describe('CreaProyectoComponent', () => {
   let component: CreaProyectoComponent;
   let fixture: ComponentFixture<CreaProyectoComponent>;
--- Frontend/src/app/crea-proyecto/crea-proyecto.component.ts
+++ Frontend/src/app/crea-proyecto/crea-proyecto.component.ts
@@ -6,6 +6,9 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ApiService } from '../services/api.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common'
+
+
 
 @Component({
   selector: 'app-crea-proyecto',
@@ -15,13 +18,18 @@ import { Router } from '@angular/router';
   styleUrl: './crea-proyecto.component.css'
 })
 export class CreaProyectoComponent {
-  getJsonValue: any;
+  
+  data:any;
 
   constructor(private router: Router, private http: HttpClient,private apiService: ApiService){
 
   }
+  ngOnInit(): void {
+    this.getclientes("opciones");
+  }
   creaproyecto(){
 
+    const idcliente = parseInt((<HTMLInputElement>document.getElementById('opciones')).value);
     const nombreproyecto = (<HTMLInputElement>document.getElementById('nombreproyecto')).value;
     const tipo = (<HTMLInputElement>document.getElementById('tipoproyecto')).value;
     const estado = (<HTMLInputElement>document.getElementById('estadoproyecto')).value;
@@ -33,18 +41,26 @@ export class CreaProyectoComponent {
     }
 
     alert("Se va a crear el proyecto: " + nombreproyecto);
-    this.apiService.postProyectosFromAPI(1,body.nombre,body.tipo,body.estado);
+    this.apiService.postProyectosFromAPI(idcliente,body.nombre,body.tipo,body.estado);
     console.log("Se deberia haber creado un nuevo proyecto");
     this.router.navigate(['dashboard']);
 }
 
-getcliente(){
+getclientes(opciones: string){
+  const options = document.getElementById(opciones) as HTMLOptionElement;
   this.apiService.getDataClientesFromAPI().subscribe((data: any) =>{
-    this.getJsonValue = data;
-    for (let i = 0; i < this.getJsonValue.length; i++) { 
-      
+  if(options != null){
+    for (let i = 0; i < data.length; i++) {
+      const option = document.createElement('option');
+      console.log(data[i]);
+      option.value = data[i].id; // Asigna el valor de la propiedad id del cliente como valor del option
+      option.textContent = data[i].nombre; // Asigna el nombre del cliente como texto del option
+      options.appendChild(option); // Agrega el option al elemento select
     }
-  })
-}
+    
+  }
+    });
+    
+ }
 
 }
